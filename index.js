adb41cce9836120207f3e84b154e159fcfc649e7
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-06-28 16:40:14 -0400

    added username and typing functions

--- index.js
+++ index.js
@@ -3,21 +3,39 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/index.html');
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket) {
+// connection event is triggered when the io() is called
+// on the client side
+io.on('connection', function(clientSocket){
   console.log('a user connected');
-  socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
-    console.log("message: " + msg);
+
+  clientSocket.on('client-join', function(username) {
+    clientSocket.username = username;
+    clientSocket.broadcast.emit('join', username);
   });
-  // socket.on('disconnect', function() {
-  //   console.log('user disconnected');
-  // });
+  // disconnect event is triggered when the user closes
+  // the browser tab, for example
+  clientSocket.on('disconnect', function() {
+    io.emit('leave', clientSocket.username);
+  });
+
+  // listen for chat messages
+  clientSocket.on('message-to-server', function(msg){
+    clientSocket.broadcast.emit('message-to-client', msg);
+  });
+
+  clientSocket.on('start-type', function(username) {
+    clientSocket.broadcast.emit('start-type', username);
+  });
+
+  clientSocket.on('end-type', function(username) {
+    clientSocket.broadcast.emit('end-type', username);
+  });
 });
 
-http.listen(3000, function() {
-  console.log('listening on port 3000');
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
