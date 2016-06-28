e8bca0149f46e281e8c5da0ee387b03dee901b5f
DeeAnn Kendrick <deeannkcooper@bellsouth.net>
2016-06-28 10:22:37 -0400

    inital commit

---
+++ index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+    console.log("message: " + msg);
+  });
+  // socket.on('disconnect', function() {
+  //   console.log('user disconnected');
+  // });
+});
+
+http.listen(3000, function() {
+  console.log('listening on port 3000');
+});
